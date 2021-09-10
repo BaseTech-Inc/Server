ea58ccaf01cc5e2642089a8834e81f65dd26c7ec
Guilherme Ivo <guilhermeivob@gmail.com>
2021-09-10 15:24:03 -0300

    update

--- src/Infrastructure/Identity/IdentityService.cs
+++ src/Infrastructure/Identity/IdentityService.cs
@@ -116,7 +116,7 @@ namespace Infrastructure.Identity
                 if (resultCreateAppUser.Succeeded)
                 {
                     var tokenEmail = await _tokenService.GenerateTokenEmail(appUser.Id);
-                    var url = $"{ _configuration["JWT:ValidIssuer"] }api/Account/verify-email?userId=" + HttpUtility.UrlEncode(usuario.Id) + "&tokenEmail=" + HttpUtility.UrlEncode(tokenEmail);
+                    var url = $"{ _configuration["baseUrl"] }api/Account/verify-email?userId=" + HttpUtility.UrlEncode(usuario.Id) + "&tokenEmail=" + HttpUtility.UrlEncode(tokenEmail);
 
                     await _emailService.SendEmailAsync(
                         appUser.Email,
