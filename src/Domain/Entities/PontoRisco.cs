1c8da84ed485273b51f8da9a2696b46f6e251320
Guilherme Ivo <guilhermeivob@gmail.com>
2021-09-24 12:04:47 -0300

    Criado entidade ponto risco #8

---
+++ src/Domain/Entities/PontoRisco.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class PontoRisco
+    {
+        public string Id { get; set; }
+
+        public string Descricao { get; set; }
+
+        public Distrito Distrito { get; set; }
+
+        public Ponto Ponto { get; set; }
+    }
+}
